4c80372c97263bbdf6ed7b3ccde75d8464cdf213
command-bat <tiagolaure@gmail.com>
2026-05-18 00:20:01 -0300

    debug

--- backend/routes/files.js
+++ backend/routes/files.js
@@ -3,7 +3,7 @@ const multer = require("multer");
 const fs = require("fs");
 const path = require("path");
 
-const { nanoid } = require("nanoid");
+const { nanoid } = await import("nanoid");
 
 const bcrypt = require("bcrypt");
 
